aac7a2010d44bf7fe49dff247735f8005a02cab2
olga-tsareko <olga.tsarenko7@gmail.com>
2017-01-28 23:20:39 +0200

    added media

--- source-js/main.js
+++ source-js/main.js
@@ -2,7 +2,19 @@ $(document).ready(function () {
     $(".industry-carousel").owlCarousel(
         {
             items: 4,
-            margin: 27
+            margin: 27,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:2
+                },
+                1000:{
+                    items:3
+                }
+            }
+
 
         }
     );
@@ -13,7 +25,18 @@ $(document).ready(function () {
             margin: 60,
             nav: true,
         navText:["",""],
-            dots: false
+            dots: false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
 
         }
     );
